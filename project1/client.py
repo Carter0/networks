2548eeeceb23c4c112e4170b0f3e372891a248e7
carter <weinbergcarter@gmail.com>
2020-01-17 19:37:08 -0500

    Adding projet updates

--- project1/client.py
+++ project1/client.py
@@ -1,5 +1,26 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("cbw.sh", 27993))
-s.send("cs3700spring2019 HELLO 001702072\n")
+s.connect(("fring.ccs.neu.edu", 27993))
+s.send("cs3700spring2020 HELLO 001702072\n".encode())
+
+
+while True:
+    msg = ""
+
+    #Read in everything up to the newline 
+    while not msg.endswith('\n'):
+        msg += s.recv(8192).decode()
+        
+    splitString = msg.split()
+    if splitString[1] == "FIND": 
+        print(msg)
+        toFind = splitString[2]
+        foundCount = splitString[3].count(toFind)
+        s.send(f'cs3700spring2020 COUNT {foundCount}'.encode()) 
+
+    if splitString[1] == "BYE":
+        print("done")
+        break 
+
+    
